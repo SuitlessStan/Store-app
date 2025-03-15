35af75bb1a37f5f83f34757d124e2768d6bb4629
Issam Olwan <52406309+SuitlessStan@users.noreply.github.com>
2025-03-15 14:05:12 +0000

    edit cart resource

--- app/Http/Resources/CartResource.php
+++ app/Http/Resources/CartResource.php
@@ -15,7 +15,6 @@ class CartResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
             'items' => $this->items,
         ];
     }
